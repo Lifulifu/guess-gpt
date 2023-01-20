0d9ebd2accc2343bbb4b7f4eb4087dd60ec1e773
Lifulifu <andycscsmaple@gmail.com>
2023-01-20 23:22:56 +0800

    add tailwind, some frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,59 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	export let data: PageData;
+	const theThing = '🤔';
+	let textInputDom: HTMLInputElement;
+	let textInputValue: string = theThing;
+
+	function addTheThing() {
+		const startPos = textInputDom.selectionStart;
+		const endPos = textInputDom.selectionEnd;
+		if (startPos === null || endPos === null) {
+			textInputValue += theThing;
+		} else {
+			textInputValue =
+				textInputValue.substring(0, startPos) +
+				theThing +
+				textInputValue.substring(endPos, textInputValue.length);
+		}
+		textInputDom.focus();
+	}
+
+	function askQuestion() {
+		console.log('askQuestion');
+		textInputDom.value = '';
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>{data.title}</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- chatroom  -->
+<div class="bg-slate-50">
+	<div class="m-auto max-w-2xl h-screen px-4 py-8 flex flex-col gap-2">
+		<div class="flex-grow" />
+
+		<div class="flex gap-2">
+			<button
+				class="rounded-full px-4 py-2 text-lg text-indigo-600 font-bold bg-white border-2 border-indigo-600 hover:brightness-90"
+				on:click={addTheThing}
+			>
+				加入"{theThing}"
+			</button>
+			<button
+				class="rounded-full px-4 py-2 text-lg text-white font-bold bg-indigo-600 hover:brightness-90"
+			>
+				我要作答
+			</button>
+		</div>
+
+		<form class="flex-shrink-0 flex" on:submit={askQuestion}>
+			<input
+				type="text"
+				bind:this={textInputDom}
+				bind:value={textInputValue}
+				class="flex-grow rounded-l-full border-2 focus:border-indigo-600 outline-none px-4 py-2"
+			/>
+			<input
+				type="submit"
+				value="我要提問"
+				class="rounded-r-full border-2 border-indigo-600 px-4 py-2 text-lg text-white font-bold bg-indigo-600 hover:brightness-90"
+			/>
+		</form>
+	</div>
+</div>
--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,8 +1,20 @@
-import type { PageServerLoad } from './$types'
+import type { RequestHandler } from './$types';
 
-export const load = ((req) => {
-  console.log(req)
-  return {
-    title: "lifu"
-  }
-}) satisfies PageServerLoad
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+// let api: ChatGPTAPIBrowser | null = null;
+
+// async function setupAPI() {
+//   console.log('setting up api')
+//   api = new ChatGPTAPIBrowser({
+//     email: process.env.CHATGPT_EMAIL as string,
+//     password: process.env.CHATGPT_PASSWORD as string,
+//   })
+//   api.initSession()
+// }
+
+export const GET = (async ({ url }) => {
+  return new Response(JSON.stringify({ res: 'fake response' }));
+}) satisfies RequestHandler;
